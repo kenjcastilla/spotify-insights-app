793bc916e0bad9ba97652d184a06c8b4d2ee5ca8
Kendall Castilla <kjcastilla+dev@gmail.com>
2025-04-10 16:00:20 -0400

    Cast Django secret key env variable to str in settings

--- spotifyInsights/settings.py
+++ spotifyInsights/settings.py
@@ -16,18 +16,13 @@ from os import getenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('DJANGO_SECRET_KEY', "django-insecure-a9^y0x43(2n_9c=e_lf7jzc84(^9i!qeg=*m7om_$13ia13#he")
+SECRET_KEY = str(getenv('DJANGO_SECRET_KEY'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
-# Application definition
 
 INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
